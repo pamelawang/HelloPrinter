76eb504d7cb3d0de30b93eacdc8271e6f793afd5
gracie <gfang@wellesley.edu>
2015-05-02 12:52:46 -0400

    printer id retrieval and other things

--- HelloPrinter.js
+++ HelloPrinter.js
@@ -1,16 +1,53 @@
 PrinterCollection = new Mongo.Collection('printers');
 //collection that holds printers' information (name, imagr URL, status, time stamp, reason for current status)
 
+
+/*only things client can change:
+    1. date. 
+    2. reason.
+    3. status.
+*/
+
+/* each printer has the following properties:
+    1. name 
+    2. timeStamp (as of last update)
+    3. reason for malfunction
+*/
+
+/* to do later:
+    1. color/bw
+    2. automatic emailing function
+*/
+
 if (Meteor.isClient) {
   Template.printerStatus.helpers({
       'printer': function(){
-            
+            console.log(PrinterCollection.find().fetch().name);
+            return PrinterCollection.find({});
         },
+      
+      'updateStatus': function() {
+          var newTime = new Date();
+          
+          
+      },
+      'showSelectedPrinter': function() {
+        var selectedPrinter = Session.get('selectedPrinter'); 
+        return PrinterCollection.findOne(selectedPrinter);
+      }
       
   }); //end helpers
 
   Template.printerStatus.events({
-    
+    'submit form': function() {
+        var printerId = this._id;
+        Session.set
+    },
+      'click .printer': function() {
+            var playerId = this._id;
+            Session.set('selectedPrinter', printerId); //returns unique printer id
+            
+        }
   }); //end events
     
 } //end client
@@ -19,6 +56,10 @@ if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
   });
+  //Meteor.publish
+  //Meteor.methods
+    
+    
 } //end server
 
 
