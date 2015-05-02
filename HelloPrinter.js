28970a927447f6561fb75fc9d4c39ee5982d8dc2
pamelawang <pamela.n.wang@gmail.com>
2015-05-02 16:16:43 -0400

    click .printer, showSelectedPrinter (helper), currentStatus (helper)
    Currently trying to get the information of the current printer from the
    `click .printer` event. This information will be sent to
    `showSelectedPrinter` (currently shows undefined). `currentStatus` is
    supposed to be a getter function like {{numPlayers}} in `leaderboard`,
    but may not need if `click .printer` works.

--- HelloPrinter.js
+++ HelloPrinter.js
@@ -21,33 +21,51 @@ PrinterCollection = new Mongo.Collection('printers');
 
 if (Meteor.isClient) {
   Template.printerStatus.helpers({
+      // 'currentStatus': function() { //trying to do something like numPLayers in leaderboard, where this returns a value and not the whole object
+      //   return showSelectedPrinter.status
+      // },
+
       'printer': function(){
             console.log(PrinterCollection.find().fetch().name);
-            return PrinterCollection.find({});
+            console.log('herro');
+            return PrinterCollection.find({})
         },
       
       'updateStatus': function() {
           var newTime = new Date();
-          
+          // var newStatus = document.getElementById(''); //id of the form submission
+          // var newReason = document.getElementById('');
           
       },
-      'showSelectedPrinter': function() {
+
+      'showSelectedPrinter': function() { //CURRENTLY IS UNDEFINED/NOT WORKING, CURRENT STRATEGY IS TO DEFINE CLICK FUNCTION TO GET THE INFO FOR THIS
         var selectedPrinter = Session.get('selectedPrinter'); 
-        return PrinterCollection.findOne(selectedPrinter);
+        return PrinterCollection.findOne(selectedPrinter) //returns the object
       }
       
   }); //end helpers
 
   Template.printerStatus.events({
     'submit form': function() {
-        var printerId = this._id;
-        Session.set
+      var printerId = this._id;
+      //Session.set
     },
-      'click .printer': function() {
-            var playerId = this._id;
-            Session.set('selectedPrinter', printerId); //returns unique printer id
-            
-        }
+    
+    'click .printer': function() { //CURRENTLY TESTING IF WE CAN GET THE NAME, LATER WILL TRY TO GET ID
+            //var playerId = this._id;
+            //Session.set('selectedPrinter', printerId); //returns unique printer id
+      console.log(this.value);
+      var selectedPrinter = Session.get('selectedPrinter');
+      //NAME ISN'T WORKING var selectedPrinterName = PrinterCollection.findOne(selectedPrinter).name;
+      console.log(selectedPrinterName);
+      // var reply = confirm('Are you sure you want to remove ' + selectedPlayerName + '?');
+      // if (reply == true) {
+      //   PlayersList.remove(selectedPlayer);
+      //   alert(selectedPlayerName + ' was removed.');
+      // } else {
+      //   alert(selectedPlayerName + ' was not removed.');
+      // }
+    }
   }); //end events
     
 } //end client
