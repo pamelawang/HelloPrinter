6ed9d5d70eccd4f198f6bc85de7ab6a0f1afb967
pamelawang <pamela.n.wang@gmail.com>
2015-05-04 20:06:47 -0400

    Making a permanent ReasonsCollection and trying to fix having Reasons appearing on page

--- HelloPrinter.js
+++ HelloPrinter.js
@@ -1,6 +1,6 @@
 PrinterCollection = new Mongo.Collection('printers');
-ReasonCollection = new Mongo.Collection('reasons');
 //collection that holds printers' information (name, imagr URL, status, time stamp, reason for current status)
+ReasonCollection = new Mongo.Collection('reasons');
 
 /*only things client can change:
     1. date. 
@@ -21,41 +21,44 @@ ReasonCollection = new Mongo.Collection('reasons');
 */
 
 if (Meteor.isClient) {
+
   Session.setDefault('printerNotWorking', false); //default is that the update form doesn't show
   Session.set('selectedPrinter');
   Session.setDefault('showForm', false);
 
   /**************************** printerStatus ****************************/
   Template.printerStatus.helpers({
-      'printer': function(){
-        //console.log(PrinterCollection.find().fetch().name);
-        //console.log('in printer function helper');
-        return PrinterCollection.find({})
-      },
+    'printer': function(){
+      //console.log(PrinterCollection.find().fetch().name);
+      //console.log('in printer function helper');
+      return PrinterCollection.find({})
+    },
 
    'showPrinterStatus': function() { 
-       //var selectedPrinter = Session.get('selectedPrinter'); 
-       //var printerStatus = PrinterCollection.findOne(selectedPrinter).status;
-       if (this.status==1) {
-           return 'printer working';
-       } else { //printer status is 0
-           return 'printer died';
-       }
+      //var selectedPrinter = Session.get('selectedPrinter'); 
+      //var printerStatus = PrinterCollection.findOne(selectedPrinter).status;
+      if (this.status==1) {
+        return 'printer working';
+      } else { //printer status is 0
+        return 'printer died';
+      }
         //return PrinterCollection.findOne(selectedPrinter); //returns the object
       }, 
-      'showPrinterTime': function() {
-          console.log(this);
-          return this.timeStamp
-          //var selectedPrinter = Session.get('selectedPrinter'); 
-          //return timeStamp = PrinterCollection.findOne(selectedPrinter).timeStamp;
-      }, 
-      'updateStatus': function() { //?????WORKING on this
-        console.log('in updateStatus');
-        // var newStatus = document.getElementById(''); //id of the form submission
-        // var newReason = document.getElementById('');
-        var thisPrinter = this;
-        //var newTime = new Date();
-      }
+
+    'showPrinterTime': function() {
+      console.log('in showPrinterTime' + this);
+      return this.timeStamp
+      //var selectedPrinter = Session.get('selectedPrinter'); 
+      //return timeStamp = PrinterCollection.findOne(selectedPrinter).timeStamp;
+    }, 
+
+    'updateStatus': function() { //?????WORKING on this
+      console.log('in updateStatus');
+      // var newStatus = document.getElementById(''); //id of the form submission
+      // var newReason = document.getElementById('');
+      var thisPrinter = this;
+      //var newTime = new Date();
+    }
   }); //end helpers
 
   Template.printerStatus.events({
@@ -67,15 +70,11 @@ if (Meteor.isClient) {
       var printerId = this._id;
     },
     
-    'click .printer': function() { //CURRENTLY TESTING IF WE CAN GET THE NAME, LATER WILL TRY TO GET ID
-<<<<<<< HEAD
+    'click .printer': function() {
       var isFormShowing = Session.get('showForm');
       console.log(isFormShowing);
 
-      if (isFormShowing) { //if the form isn't displayed
-        console.log('in if statement of click.[rinter');
-      } else {
-        console.log('ELSE click.printer');
+      if (!isFormShowing) {
         Session.set('showForm', true); //show the form
       }
     } //end events
@@ -83,30 +82,32 @@ if (Meteor.isClient) {
 
   /**************************** body ****************************/
   Template.body.helpers({
-
     'isFormDisplayed': function(){
-        var a = Session.get('showForm');
-        console.log('session currntly is ' + a);
-        return a
-    },
+        return Session.get('showForm')
+    }
+  }); //end helpers
 
+/**************************** form ****************************/
+  Template.form.helpers({
     'printerNotWorking': function(){
-      //console.log('in printerNotWorking')
-      return Session.get('printerNotWorking')
+      console.log('in printerNotWorking')
+      var a = Session.get('printerNotWorking');
+      return a
     },
 
     'reason': function() {
       return ReasonCollection.find({});  
     },
+
     'selectedClass': function() {
-        var reasonId = this._id;
-        var selectedReason = Session.get('selectedReason');
-        if(reasonId==selectedReason) 
-            return "highlight"; //this refers to a CSS class
+      var reasonId = this._id;
+      var selectedReason = Session.get('selectedReason');
+      if(reasonId==selectedReason) 
+        return "highlight"; //this refers to a CSS class
     }
   }); //end helpers
 
-  Template.body.events({
+  Template.form.events({
     'click .location': function(){
       var newLocation = this.value;
       return Session.set('selectedPrinter', newLocation)
@@ -127,15 +128,19 @@ if (Meteor.isClient) {
             var selectedReason = Session.get('selectedReason');
       }
   }); //end events
-} //end body
+} //end client
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
-    // code to run on server at startup
+    if (ReasonCollection.find({}).count() == 0){ //if empty, add options
+      var allReasons = ['Out of paper', 'Out of ink', 'Paper jam', 'Other: ', 'I don\'t know' ];
+      for (var i = 0; i < allReasons.length; i++){ //adding to collection
+        ReasonCollection.insert({reason: allReasons[i]});
+      }
+      //PAM IS HERE add <input type=test>
+    }
   });
   //Meteor.publish
   //Meteor.methods
-    
 } //end server
-
 //KEEP AUTOPUBLISH ON FOR THIS CLASS
